6250645b313c43e3bd9483096177939e12763cae
UpdateAudio <158035263+UpdateAudio@users.noreply.github.com>
2024-03-01 14:07:43 +0530

    Create ring.js

---
+++ ring.js
@@ -0,0 +1,41 @@
+ 
+    function startCountdown(buttonElement, downloadLinkContainer) {
+        let seconds = 10;
+        const countdownInterval = setInterval(function () {
+            buttonElement.innerText = `Please Wait: ${seconds} seconds`;
+            seconds--;
+
+            if (seconds < 0) {
+                clearInterval(countdownInterval);
+                buttonElement.style.display = 'none';
+                downloadLinkContainer.style.display = 'block';
+            }
+        }, 1000);
+    }
+    function downloadFile(fileUrl, fileName) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', fileUrl, true);
+        xhr.responseType = 'blob';
+
+        xhr.onload = function () {
+            var blob = new Blob([xhr.response], { type: 'audio/mpeg' });
+
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = fileName;
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
+
+        xhr.send();
+    }
+    function handleDownloadButtonClick(buttonElement, downloadLinkContainer, fileUrl, fileName) {
+        startCountdown(buttonElement, downloadLinkContainer);
+    }
+
+    // Function to handle congratulations link click
+    function handleCongratulationsClick(downloadLinkContainer, fileUrl, fileName) {
+        downloadFile(fileUrl, fileName);
+    }
